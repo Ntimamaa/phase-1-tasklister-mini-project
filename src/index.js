02970ae765277add41b39f56b3c61a68fb12bbe4
Drake Tempes <dreake.ntimama@student.moringaschool.com>
2023-06-22 11:23:18 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const value = e.target.elements['new-task-description'].value;
+    newTask(value);
+    e.target.reset();
+  });
+
+  function newTask(value) {
+    const p = document.createElement('p');
+    p.textContent = value;
+    document.querySelector('#tasks').appendChild(p);
+  }
+  document.getElementById('delete')
+  button.addEventListener('click',(task)=>{
+    task.target.parentNode.remove()
+  })
+
 });
+
